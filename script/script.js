d9238e4e9ef13d10600aec35955cfd6e00911311
GREATkuzya Дмитрий Кузовлев <greatkuzya@gmail.com>
2022-04-17 00:32:50 +0200

    add download

--- script/script.js
+++ script/script.js
@@ -8,7 +8,8 @@ btnNode.addEventListener('click', () => {
 if ( a >= 100 && a <= 3000 && b >=100 && b <=3000 ) {
     fetch(`https://picsum.photos/${a}/${b}`)
       .then((response) => { console.log('response', response.url);
-      divNode.innerHTML = `<img src="${response.url}" alt="">`;
+      divNode.innerHTML = `<img src="${response.url}" alt="">
+	  <a href="${response.url}" download>Скачать картинку</a>`;
         return response.json();
       })
       .catch(() => { console.log('error') });
